a5c78bf02c814c5f7c19558a93bd5bea9cc965c4
matteo.sinico <matteo.sinico@pwc.com>
2019-05-18 23:02:29 +0200

    added mobile server for deploy

--- poc_config/config.js
+++ poc_config/config.js
@@ -5,5 +5,6 @@ module.exports = {
   eid_provider_port: 60005,
   myhealth_port: 60009,
   vcreader_port: 60010,
-  callback_endpoint: "http://blockchaincc.ga"
+  callback_endpoint: "http://blockchaincc.ga",
+  mobileApp_port: 60011,
 };
