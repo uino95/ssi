5a80a970824b5b9f71aa0182a726ddbf8d461e87
Andrea Taglia <andrea.taglia@pwc.com>
2019-07-05 11:06:07 +0200

    minor fixes

--- pistis/models/VerifiableCredentialStatus.js
+++ pistis/models/VerifiableCredentialStatus.js
@@ -1,51 +1,30 @@
 'use-strict'
 const statusRegistry = require('../contracts/statusRegistry')
-const VerifiableCredential = require('VerifiableCredential.js')
 
 class VerifiableCredentialStatus {
   //takes a VerifiableCredential object as param
-  constructor(vc, tcl) {
-    if (!vc instanceof VerifiableCredential){
-      throw "vc needs to be a VerifiableCredential object"
-    }
-    if (!tcl instanceof TrustedContactsList) {
-      throw "tcl needs to be a TrustedContactsList"
-    }
+  constructor(vc) {
     this.vc = vc
-    this.tcl = tcl
   }
 
   checkExpiry() {
     if (this.vc.exp < (new Date().getTime())) {
-      this.status.exp = true
+      this.expired = true
     } else {
-      this.status.exp = false
+      this.expired = false
     }
   }
 
   checkSenderSubMatch(sender) {
     if (this.vc.sub == sender) {
-      this.status.sender = true
+      this.senderSubMatch = true
     } else {
-      this.status.sender = false
+      this.senderSubMatch = false
     }
   }
 
-  async checkSubjectEntity(){
-    this.status.sub = await tcl.resolveEntity(this.vc.iss)
-  }
-
-  //check if issuer is in TCL
-  async checkIssuerEntity(){
-    this.status.ent = await tcl.resolveEntity(this.vc.iss)
-  }
-
-  async checkRevocationStatus(){
-    this.status.csl = await statusRegistry.getCredentialStatus(this.vc.iss, this.vc.csl.id)
-  }
-
-  getStatus(){
-    return this.status
+  async checkStatus(){
+    this.credStatus = await statusRegistry.getCredentialStatus(this.vc.iss, this.vc.csl.id)
   }
 
 }
